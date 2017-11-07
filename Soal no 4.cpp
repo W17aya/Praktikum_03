0fae683b757864528c946abfdc1b7664ffc0fc9f
Eko Wijaya <ekowijayawindows@gmail.com>
2017-11-07 17:16:25 +0800

    Soal no 4

---
+++ Soal no 4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float integer;
+integer = 0;
+cout << "Masukkan angka :  \n" << endl;
+cin >> integer;
+
+if
+(integer > 0 )
+
+cout << "Angka yang anda masukkan adalah bilangan positif  \n" << endl;
+
+else ;
+
+if 
+(integer < 0 )
+
+cout << "Angka yang anda masukkan adalah bilangan negative  \n" << endl;
+
+else ;
+
+if
+(integer == 0)
+cout << "Angka yang anda masukkan adalah angka nol \n" << endl;
+
+else;
+
+
+return 0;9
+}
+
